dfd443f501b01a723cd69283a9aa81e2d49f75f9
takiguchi-yu <takiguchi-yu@gnavi.co.jp>
2023-02-16 11:33:35 +0900

    Next.js tRPCクライアントを作成する

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import "react-toastify/dist/ReactToastify.css";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ToastContainer } from "react-toastify";
+import { trpc } from "~/utils/trpc";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ToastContainer />
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default trpc.withTRPC(MyApp);
